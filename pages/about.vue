49139324253577242c0e0a84aaa6350902634639
alex baboy <alexbaboy2016@gmail.com>
2021-09-02 00:45:45 +0300

    route

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>About page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aut, beatae distinctio dolores eligendi eum excepturi
+      harum impedit minima nihil nostrum quam quidem quisquam quod ratione sint velit! Deserunt!</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "about"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/users/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>User {{$route.params.id}}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "_id",
+  validate() {
+    return true
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
